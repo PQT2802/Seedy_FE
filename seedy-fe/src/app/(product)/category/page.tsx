8703be365ba28270cdcdcb96f0f26043eb5a42d2
Thang <pqt2802@gmail.com>
2025-02-25 09:57:06 +0700

    add

---
+++ seedy-fe/src/app/(product)/category/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/header/header";
+import React from "react";
+import styles from "./category.module.css";
+import { styles } from "@/app/(user)/information/information.module.css";
+export default function Category() {
+  return;
+
+  <div className={styles.container}>
+    <Header />
+    <div className={styles.categoryType}>categoryType</div>
+    <div className={styles.list}>list</div>
+  </div>;
+}
