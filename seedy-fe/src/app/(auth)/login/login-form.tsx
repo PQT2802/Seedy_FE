a2fd747a1943c9f88700e623c7507079b6bba731
Thang <pqt2802@gmail.com>
2025-03-11 14:46:36 +0700

    Merge branch 'thang_3' into thang_2
    # Conflicts:
    #	seedy-fe/src/app/(auth)/login/login-form.tsx

--- seedy-fe/src/app/(auth)/login/login-form.tsx
+++ seedy-fe/src/app/(auth)/login/login-form.tsx
@@ -50,7 +50,7 @@ export default function LoginForm() {
   const [isLoading, setIsLoading] = React.useState(false);
   const [error, setError] = React.useState("");
   const [showPassword, setShowPassword] = React.useState(false);
-
+  const router = useRouter();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: { email: "", password: "", rememberMe: false },
